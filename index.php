b4906f4f9692edbc1fc2e8f882f9d0da0d6a8032
pascaaaal <45233618+pascaaaal@users.noreply.github.com>
2018-11-22 16:43:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,9 @@ if ($_SERVER['REQUEST_METHOD'] == "GET"){
         $base = file_get_contents("php://input");
         $wdata = json_decode($base);
         if($wdata != null){
-            echo $wdata;
+            sendMessage(array("d" => $wdata));
             return;
             if(isset($wdata["temperature"]) && isset($wdata["humidity"]) && isset($wdata["uvindex"]) && isset($wdata["air_pressure"]) && isset($wdata["illumiance"])){
-                sendMessage(array("d" => true, "key" => $newkey));
-                return;
                 $temp = $wdata["temperature"];
                 $huminity = $wdata["humidity"];
                 $uvindex = $wdata["uvindex"];
