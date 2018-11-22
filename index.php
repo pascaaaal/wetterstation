8d37b088459beac2b93affc380901a65f4222808
pascaaaal <45233618+pascaaaal@users.noreply.github.com>
2018-11-22 17:02:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -48,6 +48,6 @@ function generateRandomString($length){
 		$rand = mt_rand(0, $max);
 		$str .= $characters[$rand];
 	}
-	sendMessage(array("d" => $str);
+	sendMessage(array("d" => $str));
 	return $str;
 }
