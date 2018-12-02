daf896e0d431b37cb6a9378dfd27a8863b39eee7
pascaaaal <45233618+pascaaaal@users.noreply.github.com>
2018-12-02 13:13:58 +0100

    Create bugreport.php

---
+++ bugreport.php
@@ -0,0 +1,30 @@
+<?php
+set_time_limit(1000);
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+    sendError(0, "Invalid Request");
+}elseif ($_SERVER['REQUEST_METHOD'] == "PUT"){
+    $base = file_get_contents("php://input");
+    $data = json_decode($base);
+    
+    $device = $data->device;
+    $type = $data->type;
+    $message = $data->message;
+    
+    if($device == "mobile"){
+        $mmessage = "Bugreport from Mobile App:\r\n" . $message;
+        mail("pascal.faude@gmail.com", "Weatherstation Bugreport!", $mmessage);
+    }else{
+    
+    }
+    
+}else{
+    sendError(0, "Invalid Request");
+}
+function sendError($type, $message){
+    sendMessage(array("error" => true, "type" => $type, "message" => $message));
+}
+function sendMessage($data){
+    header("Content-Type: application/json");
+    header("Content-Length: " . strval(strlen(json_encode($data))));
+    echo json_encode($data);
+}
